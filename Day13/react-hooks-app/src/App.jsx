1bf7c1a05247c61cbd1d460282b5dd68baa4c07e
radhikaguptaaa <radhikaaaguptaa36@gmail.com>
2024-12-04 12:14:22 +0530

    commit

--- Day13/react-hooks-app/src/App.jsx
+++ Day13/react-hooks-app/src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-// import CounterApp from "./Components/CounterApp";
-import ImageApp from './Components/ImageApp';       
+import CounterApp from "./Components/CounterApp";
+import ImageApp from './Components/ImageApp';     
+import {BrowserRouter , Routes , Route} from 'react-router-dom';  
+import MainLayout from './Components/MainLayout';
 
 const App = () => {
   return (
-    <>
-      {/* <CounterApp />  */}
-      <ImageApp />
-    </>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path ="/" element = {<MainLayout />}>
+        <Route path ="/CounterApp" element = {<CounterApp />} />
+        <Route path ="/ImageApp" element = {<ImageApp />} />
+
+        </Route>
+      </Routes>
+      </BrowserRouter>
+    </div>
   )
 };
 
